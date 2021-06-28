f8fd1f9f04a674c7d592caa274f74a3b017e6d97
Masekela Malematsa <masekela.malematsa@stonethree.com>
2021-06-28 19:29:53 +0200

    Update contact.php
    Added mail logs

--- forms/contact.php
+++ forms/contact.php
@@ -10,7 +10,9 @@
   $receiving_email_address = 'mase@mvo.digital';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    print("Before init mailer");
     include( $php_email_form );
+    print("After init mailer");
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
