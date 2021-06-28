4c23791a9a7b97282525be984ba1ec88116c1f40
Masekela Malematsa <masekela.malematsa@stonethree.com>
2021-06-28 20:39:05 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -24,20 +24,18 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'himorakenshin123@gmail.com',
+    'password' => 'Onimusha123',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  print("Before init mailer");
   echo $contact->send();
-  print("After init mailer");
 ?>
